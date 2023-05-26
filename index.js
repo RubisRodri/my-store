ee9158e68ec780bca50e3475202692b7115283ca
Rubis Rodriguez <rubisrodri@gmail.com>
2023-05-26 17:59:02 -0500

    new scripts

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('my application')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>{
+  res.send('Hola mi server listening')
+})
+
+app.listen(port, ()=>{
+  console.log('mi port' + port)
+});
